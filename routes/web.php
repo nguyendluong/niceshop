629862cbc770b8d3466b4c8d73e45e7f46d4237b
Dat Tran <dat.tq@hodfords.com>
2022-09-18 11:18:19 +0700

    update web router

--- routes/web.php
+++ routes/web.php
@@ -1,83 +1,52 @@
 <?php
 
+use App\Http\Controllers\User\Auth\LoginController;
+use App\Http\Controllers\User\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
+Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('login');
+// view login return view ('login.index');
+Route::middleware('auth')->group(function (){
+    // Router with authentication example /cart, /payment, /my-order,...
+});
+
+Route::get('/register', function () {
+    return view ('register.index');
+});
+
 
 Route::get('/', function () {
     return view('client.homepage.index');
 });
 
-Route::group(['prefix' => '/client'], function () {
-    Route::get('/', function () {
-        return view('client.homepages.index');
-    });
+Route::get('/client', function () {
+    return view('client.homepages.index');
 });
-Route::group(['prefix' => '/cart'], function () {
-    Route::get('/', function () {
+
+Route::get('/cart', function () {
     return view ('client.cart.index');
-    });
-
 });
-Route::group(['prefix' => '/news'], function () {
-    Route::get('/', function () {
+
+Route::get('/news', function () {
     return view ('client.news.index');
-    });
-
 });
-Route::group(['prefix' => '/news-detail'], function () {
-    Route::get('/', function () {
+
+Route::get('/news-detail', function () {
     return view ('client.news-detail.index');
-    });
-
 });
-Route::group(['prefix' => '/checkout'], function () {
-    Route::get('/', function () {
+
+Route::get('/checkout', function () {
     return view ('client.checkout.index');
-    });
-
 });
-Route::group(['prefix' => '/product-detail'], function () {
-    Route::get('/', function () {
+
+Route::get('/product-detail', function () {
     return view ('client.product-detail.index');
-    });
-
 });
-Route::group(['prefix' => '/product'], function () {
-    Route::get('/', function () {
+
+Route::get('/product', function () {
     return view ('client.product.index');
-    });
-
 });
-Route::group(['prefix' => '/product-list'], function () {
-    Route::get('/', function () {
+
+Route::get('/product-list', function () {
     return view ('client.product-list.index');
-    });
-
-});
-Route::get('/', [App\Http\Controllers\OrderController::class, 'index']);
-
-
-Route::group(['prefix' => '/login'], function () {
-    Route::get('/', function () {
-        return view ('login.index');
-        });
-
-});
-
-
-Route::group(['prefix' => '/register'], function () {
-    Route::get('/', function () {
-        return view ('register.index');
-        });
-
 });
